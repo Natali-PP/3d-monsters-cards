2665c4a45d92fd2cc1eb96d528ae530fea9f262d
Natali Palacio Pastor <cristinanatalip@gmail.com>
2023-07-09 10:31:56 -0300

    Added tilting interaction between the mouse and the card component

--- src/components/CardPortal.jsx
+++ src/components/CardPortal.jsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
 import { useFrame } from "@react-three/fiber";
 import * as easing from "maath/easing";
 import { Environment, MeshPortalMaterial, RoundedBox } from "@react-three/drei";
@@ -11,14 +11,58 @@ const CardPortal = ({
   setActive,
   ...props
 }) => {
+  const [mouseCoords, setMouseCoords] = useState({ x: 0, y: 0 })
   const portalMaterial = useRef();
+  const roundedBoxRef = useRef();
+  const mouseTolerance = 0.2;
+
+  useEffect(() => {
+    const handleWindowMouseMove = event => {
+      setMouseCoords({
+        x: event.clientX,
+        y: event.clientY,
+      });
+    };
+    window.addEventListener('mousemove', handleWindowMouseMove);
+
+    return () => {
+      window.removeEventListener(
+        'mousemove',
+        handleWindowMouseMove,
+      );
+    };
+  }, []);
+
+  const centerX = window.innerWidth * 0.5;
+  const centerY = window.innerHeight * 0.5;
+
   useFrame((_state, delta) => {
     const isWorldOpen = active === name;
-    easing.damp(portalMaterial.current, 'blend', isWorldOpen ? 1 : 0, 0.15, delta)
+    easing.damp(portalMaterial.current, 'blend', isWorldOpen ? 1 : 0, 0.15, delta);
+
+    if (!active) {
+      roundedBoxRef.current.rotation.x = (mouseCoords.y - centerY) / centerY * mouseTolerance;
+      roundedBoxRef.current.rotation.y = (mouseCoords.x - centerX) / centerX * mouseTolerance;
+    }
   })
+
   return (
-    <group {...props} >
-      <RoundedBox args={[2, 3, 0.2]} radius={0.2} onDoubleClick={() => setActive(active === name ? null : name)} name={name}>
+    <group {...props}
+
+    >
+      <RoundedBox
+        args={[2, 3, 0.2]}
+        radius={0.2}
+        onDoubleClick={() => setActive(active === name ? null : name)}
+        name={name}
+        ref={roundedBoxRef}
+        onPointerMove={({ clientX, clientY }) => {
+          setMouseCoords({
+            x: clientX,
+            y: clientY
+          })
+        }}
+      >
         <MeshPortalMaterial side={THREE.DoubleSide} ref={portalMaterial}>
           <ambientLight intensity={2.5} />
           <Environment preset="sunset" />
