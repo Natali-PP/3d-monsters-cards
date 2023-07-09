b4ca0426c19feb52911cd8fee549c39849cc58ae
Natali Palacio Pastor <cristinanatalip@gmail.com>
2023-07-09 18:26:52 -0300

    Better performance in tilt and rotation for the card portal. Fixed initial choppy behaviour

--- src/components/CardPortal.jsx
+++ src/components/CardPortal.jsx
@@ -1,9 +1,9 @@
 import { useRef, useState, useEffect } from "react";
 import { useFrame } from "@react-three/fiber";
 import * as easing from "maath/easing";
-import { Environment, MeshPortalMaterial, RoundedBox } from "@react-three/drei";
+import { Environment, MeshPortalMaterial, RoundedBox, Sparkles, Trail } from "@react-three/drei";
 import * as THREE from 'three';
-
+import { motion } from "framer-motion-3d"
 const CardPortal = ({
   children,
   name,
@@ -14,7 +14,9 @@ const CardPortal = ({
   const [mouseCoords, setMouseCoords] = useState({ x: 0, y: 0 })
   const portalMaterial = useRef();
   const roundedBoxRef = useRef();
-  const mouseTolerance = 0.2;
+  const mouseTolerance = 0.5;
+  const centerX = window.innerWidth * 0.5;
+  const centerY = window.innerHeight * 0.5;
 
   useEffect(() => {
     const handleWindowMouseMove = event => {
@@ -33,22 +35,30 @@ const CardPortal = ({
     };
   }, []);
 
-  const centerX = window.innerWidth * 0.5;
-  const centerY = window.innerHeight * 0.5;
-
   useFrame((_state, delta) => {
     const isWorldOpen = active === name;
     easing.damp(portalMaterial.current, 'blend', isWorldOpen ? 1 : 0, 0.15, delta);
 
+
     if (!active) {
-      roundedBoxRef.current.rotation.x = (mouseCoords.y - centerY) / centerY * mouseTolerance;
-      roundedBoxRef.current.rotation.y = (mouseCoords.x - centerX) / centerX * mouseTolerance;
+      easing.dampE(roundedBoxRef.current.rotation, [
+        (mouseCoords.y - centerY) / centerY * mouseTolerance,
+        (mouseCoords.x - centerX) / centerX * mouseTolerance,
+        0
+      ], 0.25, delta)
     }
   })
 
   return (
-    <group {...props}
-
+    <motion.group {...props}
+      initial="hidden"
+      animate="visible"
+      viewport={{ once: false }}
+      transition={{ duration: 2.3 }}
+      variants={{
+        visible: { opacity: 1 },
+        hidden: { opacity: 0 }
+      }}
     >
       <RoundedBox
         args={[2, 3, 0.2]}
@@ -62,14 +72,16 @@ const CardPortal = ({
             y: clientY
           })
         }}
+
       >
         <MeshPortalMaterial side={THREE.DoubleSide} ref={portalMaterial}>
           <ambientLight intensity={2.5} />
-          <Environment preset="sunset" />
           {children}
         </MeshPortalMaterial>
       </RoundedBox>
-    </group>
+
+      <Sparkles count={80} scale={2} speed={0.4} size={5} />
+    </motion.group>
   )
 }
 
