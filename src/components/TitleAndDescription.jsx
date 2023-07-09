34d9a4264847eb99f795437eb3a791ae21d9de79
Natali Palacio Pastor <cristinanatalip@gmail.com>
2023-07-09 18:28:49 -0300

    Changed the way the title and descriptions where structures. Added more flexible styling

--- src/components/TitleAndDescription.jsx
+++ src/components/TitleAndDescription.jsx
@@ -1,13 +1,16 @@
 import { motion } from "framer-motion"
 import styles from '../styles/TitleAndDescription.module.css'
+import { useRef } from "react"
 
 const TitleAndDescription = ({ active, title, description, styleTitle, styleDescription }) => {
+  const ref = useRef();
   return (
     <>
       <motion.h1
         animate={{ color: active ? 'black' : 'white', textShadow: active ? '#b7eaf3  2px 5px' : '#cb91c1 2px 5px' }}
         transition={{ duration: 1.5 }}
         className={`${styles.title} ${styles[styleTitle]}`}
+        ref={ref}
       >
         {title}
       </motion.h1>
