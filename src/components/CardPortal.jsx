9c87b96c0ea39dee9151652b2722a19b7a9aaf1b
Natali Palacio Pastor <cristinanatalip@gmail.com>
2023-07-10 11:39:21 -0300

    Changed sparkles color and noise. Deleted unused animation. Changed both easing animations linting.

--- src/components/CardPortal.jsx
+++ src/components/CardPortal.jsx
@@ -1,9 +1,9 @@
 import { useRef, useState, useEffect } from "react";
 import { useFrame } from "@react-three/fiber";
 import * as easing from "maath/easing";
-import { Environment, MeshPortalMaterial, RoundedBox, Sparkles, Trail } from "@react-three/drei";
+import { MeshPortalMaterial, RoundedBox, Sparkles } from "@react-three/drei";
 import * as THREE from 'three';
-import { motion } from "framer-motion-3d"
+
 const CardPortal = ({
   children,
   name,
@@ -37,32 +37,33 @@ const CardPortal = ({
 
   useFrame((_state, delta) => {
     const isWorldOpen = active === name;
-    easing.damp(portalMaterial.current, 'blend', isWorldOpen ? 1 : 0, 0.15, delta);
-
+    easing.damp(
+      portalMaterial.current,
+      'blend',
+      isWorldOpen ? 1 : 0,
+      0.15,
+      delta
+    );
 
     if (!active) {
-      easing.dampE(roundedBoxRef.current.rotation, [
-        (mouseCoords.y - centerY) / centerY * mouseTolerance,
-        (mouseCoords.x - centerX) / centerX * mouseTolerance,
-        0
-      ], 0.25, delta)
+      easing.dampE(
+        roundedBoxRef.current.rotation,
+        [
+          (mouseCoords.y - centerY) / centerY * mouseTolerance,
+          (mouseCoords.x - centerX) / centerX * mouseTolerance,
+          0
+        ],
+        0.25,
+        delta
+      )
     }
   })
 
   return (
-    <motion.group {...props}
-      initial="hidden"
-      animate="visible"
-      viewport={{ once: false }}
-      transition={{ duration: 2.3 }}
-      variants={{
-        visible: { opacity: 1 },
-        hidden: { opacity: 0 }
-      }}
-    >
+    <group {...props}>
       <RoundedBox
-        args={[2, 3, 0.2]}
-        radius={0.2}
+        args={[2, 3, 0.3]}
+        radius={0.3}
         onDoubleClick={() => setActive(active === name ? null : name)}
         name={name}
         ref={roundedBoxRef}
@@ -80,8 +81,8 @@ const CardPortal = ({
         </MeshPortalMaterial>
       </RoundedBox>
 
-      <Sparkles count={80} scale={2} speed={0.4} size={5} />
-    </motion.group>
+      <Sparkles count={80} scale={2} speed={0.4} size={5} color={'red'} noise={5} />
+    </group>
   )
 }
 
