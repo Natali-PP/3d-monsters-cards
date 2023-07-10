7b65d2174a47ee6c9d7ba2b946dfd90b47f8f224
Natali Palacio Pastor <cristinanatalip@gmail.com>
2023-07-10 11:45:27 -0300

    Added texts animation that change whether the user is inside or outside the portal

--- src/components/TitleAndDescription.jsx
+++ src/components/TitleAndDescription.jsx
@@ -4,22 +4,49 @@ import { useRef } from "react"
 
 const TitleAndDescription = ({ active, title, description, styleTitle, styleDescription }) => {
   const ref = useRef();
+
+  const variantsH1 = {
+    insidePortal: {
+      color: '#000000',
+      textShadow: '3px 3px 2px #927856',
+      y: "-4vw",
+    },
+    outsidePortal: {
+      color: '#b9b18e',
+      textShadow: '#927856 2px 5px',
+      y: "4vw",
+    }
+  }
+
+  const variantsP = {
+    insidePortal: {
+      color: '#000000',
+      textShadow: '2px 2px 5px #927856',
+      y: "-4vw",
+    },
+    outsidePortal: {
+      color: '#b9b18e',
+      textShadow: '1px 1px 2px #927856',
+      y: "4vw",
+    }
+  }
   return (
     <>
       <motion.h1
-        animate={{ color: active ? 'black' : 'white', textShadow: active ? '#b7eaf3  2px 5px' : '#cb91c1 2px 5px' }}
-        transition={{ duration: 1.5 }}
+        animate={active ? "insidePortal" : "outsidePortal"}
+        variants={variantsH1}
+        transition={{ duration: 1.25 }}
         className={`${styles.title} ${styles[styleTitle]}`}
         ref={ref}
       >
         {title}
       </motion.h1>
       <motion.p
-        animate={{ color: active ? 'black' : 'white', textShadow: active ? '#b7eaf3 1px 0 10px' : '#cb91c1 2px 2px 3px' }}
-        transition={{ duration: 1.5 }}
+        animate={active ? "insidePortal" : "outsidePortal"}
+        variants={variantsP}
+        transition={{ duration: 1.25 }}
         className={`${styles.description} ${styles[styleDescription]}`}
       >
-
         {description}
       </motion.p>
     </>
