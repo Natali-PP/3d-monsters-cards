5e79aef32d75a0f4edbe4a9e6d63a377a36de8fd
Natali Palacio Pastor <cristinanatalip@gmail.com>
2023-07-11 09:25:04 -0300

    Added responsive styling for the text. Disabled the scroll animation in small devices

--- src/components/ScrollingCards.jsx
+++ src/components/ScrollingCards.jsx
@@ -1,17 +1,22 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { useScroll } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
+import { useFrame, useThree } from "@react-three/fiber";
 
 const ScrollingCards = ({ children, ...props }) => {
   const scrollRef = useRef();
   const data = useScroll();
+  const { viewport } = useThree()
+
   useFrame(() => {
-    scrollRef.current.children[0].position.x = 1 + data.offset * 3
-    scrollRef.current.children[1].position.x = -2 + data.offset * 3
-    scrollRef.current.children[2].position.x = 2.5 - data.offset * 3
-    scrollRef.current.children[3].position.x = -3.5 + data.offset * 3
-    scrollRef.current.children[4].position.x = 4 - data.offset * 3
+    if (viewport.width > 2.8) {
+      scrollRef.current.children[0].position.x = 1 + data.offset * 3
+      scrollRef.current.children[1].position.x = -2 + data.offset * 3
+      scrollRef.current.children[2].position.x = 2.5 - data.offset * 3
+      scrollRef.current.children[3].position.x = -3.5 + data.offset * 3
+      scrollRef.current.children[4].position.x = 4 - data.offset * 3
+    }
   })
+
   return (
     <group ref={scrollRef}>
       {children}
