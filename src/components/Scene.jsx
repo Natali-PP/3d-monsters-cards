52f16619e237447e9ce9c75cc66a68251312ba22
Natali Palacio Pastor <cristinanatalip@gmail.com>
2023-07-11 23:29:21 -0300

    Added texts container component. Synced the camera's position to the portal store. Deleted old props

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -9,15 +9,17 @@ import { Alien } from "./models/Alien"
 import ModelWithMapEnv from "./ModelWithMapEnv";
 import CardPortal from "./CardPortal";
 import ScrollingCards from "./ScrollingCards";
-import TitleAndDescription from "./TitleAndDescription";
 import { Demon } from "./models/Demon";
+import usePortalStore from "@/store/usePortalStore";
+import TitleandDescriptionContainer from "./TitleAndDescriptionContainer";
 
 const Scene = () => {
-  const [active, setActive] = useState(false);
   const [enableCamera, setEnableCamera] = useState(false);
   const cameraControlsRef = useRef();
   const scene = useThree((state) => state.scene);
-
+  const [active, setActive] = usePortalStore(
+    (state) => [state.active, state.setActive]
+  )
   useEffect(() => {
     if (active) {
       setEnableCamera(true)
@@ -51,118 +53,44 @@ const Scene = () => {
 
   return (
     <>
-      <CameraControls ref={cameraControlsRef}
-        enabled={enableCamera} />
+      <CameraControls ref={cameraControlsRef} enabled={enableCamera} />
       <Environment preset="sunset" />
-      <ScrollControls damping={0.5} pages={6} >
+      <ScrollControls damping={0.65} pages={6} >
         <Scroll>
           <ScrollingCards>
-            <CardPortal
-              name="demon"
-              active={active}
-              setActive={setActive}
-            >
-              <ModelWithMapEnv
-                texture='textures/demon.jpg'
-              >
+            <CardPortal name="demon">
+              <ModelWithMapEnv texture='textures/demon.jpg'>
                 <Demon scale={0.5} position={[0, -0.4, 0]} />
               </ModelWithMapEnv>
             </CardPortal>
 
-            <CardPortal
-              position={[0, -5, 0]}
-              active={active}
-              setActive={setActive}
-              name='cthulu'>
-              <ModelWithMapEnv
-                texture='textures/cthulhu.jpg'
-              >
+            <CardPortal position={[0, -5, 0]} name='cthulu'>
+              <ModelWithMapEnv texture='textures/cthulhu.jpg'>
                 <Cthulhu scale={0.5} position={[0, -0.4, 0]} />
               </ModelWithMapEnv>
             </CardPortal>
 
-            <CardPortal
-              name='alien'
-              position={[0, -10, 0]}
-              active={active}
-              setActive={setActive}
-            >
-              <ModelWithMapEnv
-                texture='textures/alien.jpg'
-              >
+            <CardPortal name='alien' position={[0, -10, 0]}>
+              <ModelWithMapEnv texture='textures/alien.jpg'>
                 <Alien scale={0.5} position={[0, -0.4, 0]} />
               </ModelWithMapEnv>
             </CardPortal>
-            <CardPortal
-              name='mushroom'
-              position={[0, -15, 0]}
-              active={active}
-              setActive={setActive}
-            >
-              <ModelWithMapEnv
-                texture='textures/mushroom2.jpg'
-              >
+
+            <CardPortal name='mushroom' position={[0, -15, 0]}>
+              <ModelWithMapEnv texture='textures/mushroom2.jpg'>
                 <Mushroom scale={0.5} position={[0, -0.4, 0]} />
               </ModelWithMapEnv>
             </CardPortal>
-            <CardPortal
-              name='bat'
-              position={[0, -20, 0]}
-              active={active}
-              setActive={setActive}
-            >
-              <ModelWithMapEnv
-                texture='textures/dracula.jpg'
-              >
+
+            <CardPortal name='bat' position={[0, -20, 0]}>
+              <ModelWithMapEnv texture='textures/dracula.jpg'>
                 <Bat scale={0.5} position={[0, -0.4, 0]} />
               </ModelWithMapEnv>
             </CardPortal>
           </ScrollingCards>
         </Scroll>
         <Scroll html>
-          <TitleAndDescription
-            active={active}
-            title="Demon"
-            description="Tap into our deepest fears and religious beliefs. What's evil, torment or eternal damnation? 
-            This realm evokes a fear of punishment, spiritual darkness, and the possibility of losing one's soul. 
-            Face the uncertainty of what lies beyond death and the dread of facing the consequences of our actions in the afterlife."
-            styleTitle="firstTitle"
-            styleDescription="firstDescription"
-          />
-          <TitleAndDescription
-            active={active}
-            title="Cthulhu"
-            description="“That is not dead which can eternal lie,
-            And with strange aeons even death may die”"
-            styleTitle="secondTitle"
-            styleDescription="secondDescription"
-          />
-          <TitleAndDescription
-            active={active}
-            title="Alien"
-            description="It's unfamiliar appearance and advanced capabilities challenge our understanding of the universe, 
-            triggering feelings of vulnerability and insignificance. Get to know their intentions, the potential for invasion 
-            or abduction, and the unsettling realization that we may not be alone in the vastness of space"
-            styleTitle="thirdTitle"
-            styleDescription="thirdDescription"
-          />
-          <TitleAndDescription
-            active={active}
-            title="Mushroom"
-            description="An eerie environment where the line between beauty and danger becomes blurred,
-            as if you had stepped into a realm where nature's secrets whispered of danger and the unknown."
-            styleTitle="fourthTitle"
-            styleDescription="fourthDescription"
-          />
-          <TitleAndDescription
-            active={active}
-            title="Drácula"
-            description="Whats more scary than his immortal existence?
-            His insatiable thirst for blood, his charisma, supernatural powers
-            - a relentless predator, immortal and insatiable"
-            styleTitle="fifthTitle"
-            styleDescription="fifthDescription"
-          />
+          <TitleandDescriptionContainer />
         </Scroll>
       </ScrollControls>
     </>
